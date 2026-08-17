3d4da9a894e3a6e1a1add62d7e30d58e8e671e8e
Sehaj38 <kaursehajneet06@gmail.com>
2026-08-17 09:18:07 +0530

    feat: set up Express server and connect to MongoDB

--- src/index.js
+++ src/index.js
@@ -5,4 +5,23 @@ dotenv.config({
     path: './.env'
 })
 
+const PORT = process.env.PORT || 8000
+
 connectDB()
+.then(() => {
+    try {
+        app.on("error", (error) => {
+            console.log("Error: ", error)
+            throw error
+        })
+        app.listen(PORT, () => {
+            log.console(`Server is running at port: ${PORT}`)
+        })
+    } catch (error) {
+        console.log("Error: ", error)
+        throw error
+    }
+})
+.catch((err) => {
+    console.log("MongoDB connection failed ", err);
+})
